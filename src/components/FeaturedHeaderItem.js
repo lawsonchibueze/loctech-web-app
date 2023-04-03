a3615e4c046b4c8211cdf889b36d67f6eb04f799
wesley-codes <ezemikey12@gmail.com>
2023-04-03 16:26:29 +0100

    made changes

--- src/components/FeaturedHeaderItem.js
+++ src/components/FeaturedHeaderItem.js
@@ -9,7 +9,7 @@ export default function FeaturedHeaderItem({title, param}) {
 
     const searchParams = useSearchParams();
     const category = searchParams.get("category");
-    console.log(category)
+    // console.log(category)
   return (
    <div className={`sm:mx-4  ${category && category === param && "shadow-lg transition-shadow duration-300 p-3"} `}>
     <Link href={`/?category=${param}`}>
--- src/components/Subjects/SubjectCard.js
+++ src/components/Subjects/SubjectCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 export default function SubjectCard({ title, img }) {
   return (
-    <div class="relative my-4 cursor-pointer">
+    <div className="relative my-4 cursor-pointer">
       <Image
         src={img}
         alt="Image"
@@ -12,12 +12,12 @@ export default function SubjectCard({ title, img }) {
         blurDataURL="/"
         className="w-full h-80 rounded-lg rounded-b-lg object-cover	"
       />
-      <div class="absolute inset-0">
-        <div class="h-full w-full bg-gradient-to-t from-black to-transparent"></div>
+      <div className="absolute inset-0">
+        <div className="h-full w-full bg-gradient-to-t from-black to-transparent"></div>
       </div>
-      <div class="absolute bottom-5 left-0 w-full">
-        <div class="flex justify-center items-center h-16 bg-black bg-opacity-5">
-          <h1 class="text-xl font-bold text-white"> {title}</h1>
+      <div className="absolute bottom-5 left-0 w-full">
+        <div className="flex justify-center items-center h-16 bg-black bg-opacity-5">
+          <h1 className="text-xl font-bold text-white"> {title}</h1>
         </div>
       </div>
     </div>
