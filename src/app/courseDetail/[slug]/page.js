7965852f2d77afdcfbd2e445a6b78fa633791f0a
Lawson Chibueze <speak2lawson@gmail.com>
2023-04-13 09:51:06 +0100

    updated course detail page

--- src/app/courseDetail/[slug]/page.js
+++ src/app/courseDetail/[slug]/page.js
@@ -6,18 +6,36 @@ import Curriculum from "@/components/Curriculum";
 import InstructorCard from "@/components/InstructorCard";
 import RatingCard from "@/components/RatingCard";
 import Reviews from "@/components/Reviews";
-export default function page({ params }) {
-     const courseId = params.slug;
-     console.log(courseId)
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const fetchCourseBySlug = async (slug) => {
+  const course = await prisma.course.findUnique({
+    where: {
+      slug: slug,
+    },
+  });
+
+  if (!course) {
+    return null;
+  }
+
+  return course;
+};
+
+export default async function page({ params }) {
+  const { slug } = params;
+  const course = await fetchCourseBySlug(slug);
+  console.log({ course });
   return (
     <div>
       <CourseDetailHero />
       <CourseDescription />
       <Curriculum />
       <InstructorCard />
-      <RatingCard/>
-      <Reviews/>
-
+      <RatingCard />
+      <Reviews />
     </div>
   );
 }
