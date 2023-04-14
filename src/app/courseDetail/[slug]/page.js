e8de2fdbf4bab21787ee9673fc3c5dfcc0a39975
Lawson Chibueze <speak2lawson@gmail.com>
2023-04-14 09:52:31 +0100

    added instructor func

--- src/app/courseDetail/[slug]/page.js
+++ src/app/courseDetail/[slug]/page.js
@@ -6,10 +6,20 @@ import Curriculum from "@/components/Curriculum";
 import InstructorCard from "@/components/InstructorCard";
 import RatingCard from "@/components/RatingCard";
 import Reviews from "@/components/Reviews";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Instructor } from "@prisma/client";
 import Instructors from "@/app/instructors/page";
 
 const prisma = new PrismaClient();
+
+const fetchInstructorById = async (instructorId) => {
+  const instructor = await prisma.instructor.findFirst({
+    where: {
+      instructorId,
+    },
+  });
+
+  return instructor;
+};
 
 const fetchCourseBySlug = async (slug) => {
   const course = await prisma.course.findUnique({
@@ -26,9 +36,11 @@ const fetchCourseBySlug = async (slug) => {
 };
 
 export default async function page({ params }) {
-  const { slug } = params;
+  const { slug, instructorId } = params;
   const course = await fetchCourseBySlug(slug);
   console.log("------", course);
+  const instructor = await fetchInstructorById(instructorId);
+  console.log(instructor);
   return (
     <div>
       <CourseDetailHero
