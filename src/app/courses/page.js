0066ec00cb8594e6ed66dc8829382d074a606fe1
wesley-codes <ezemikey12@gmail.com>
2023-04-26 10:16:17 +0100

    bug fixes and error page

--- src/app/courses/page.js
+++ src/app/courses/page.js
@@ -5,40 +5,25 @@ import FeaturedItem from "@/components/Featured/FeaturedItem";
 const prisma = new PrismaClient();
 
 const fetchRelatedCourses = async (name) => {
-  const coursescag = await prisma.course.findMany({
+  const coursesCategory = await prisma.course.findMany({
     where: {
       RelatedCourses: {
         name: {
-          equals: "cloudSecurity",
+          equals: `${name}`,
         },
       },
     },
   });
-  return coursescag;
-
-  console.log(fetchRelatedCourses);
-};
-
-const fetchCourse = async (category) => {
-  const courses = await prisma.course.findMany({
-    where: {
-      category: category,
-    },
-  });
-  return courses;
+  return coursesCategory;
 };
 
 export default async function page({ searchParams }) {
-  const category = searchParams?.category?.toUpperCase();
-  const categorizedCourses = await fetchCourse(category);
-  const relatedCourses = await fetchRelatedCourses();
-
-  // console.log(category);
-  console.log(relatedCourses);
+  const category = searchParams?.category;
+  const relatedCourses = await fetchRelatedCourses(category);
 
   return (
     <div className=" gap-4 mx-3 sm:grid sm:grid-cols-2 sm:gap-x-8 sm:mx-20 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5   py-4 ">
-      <FeaturedItem courses={categorizedCourses} />
+      <FeaturedItem courses={relatedCourses} />
     </div>
   );
 }
