990e2d3fbe753f17b3cf5fdd7241073ea6e7bcf8
Lawson Chibueze <speak2lawson@gmail.com>
2023-05-20 11:19:29 +0100

    policy edited

--- src/app/policy/page.js
+++ src/app/policy/page.js
@@ -1,63 +1,40 @@
-import React from "react";
-
 const page = () => {
   return (
     <div className="p-5 ">
       <p>
         <p className="text-2xl">Loctech Nigeria Lead Ads Policy</p>
-        <br />
-        1. Purpose The purpose of this policy is to establish guidelines and
-        standards for the use of Facebook Lead Ads by employees and
-        representatives of Loctech. Facebook Lead Ads are an effective tool for
-        generating leads and capturing customer information, and it is important
-        that their use aligns with our company values and legal requirements.
-        <br />
-        2. Scope This policy applies to all employees, contractors, and
-        representatives of Loctech who utilize Facebook Lead Ads for business
-        purposes.
-        <br />
-        3. Compliance with Laws and Regulations All Facebook Lead Ads created by
-        Loctech must comply with applicable laws, regulations, and guidelines,
-        including but not limited to data protection and privacy laws, consumer
-        protection laws, and advertising standards. It is the responsibility of
-        each employee or representative to ensure compliance with these laws and
-        regulations.
-        <br />
-        4. Authorized Use Facebook Lead Ads may only be created and managed by
-        authorized employees or representatives of Loctech who have received
-        appropriate training and have been granted access to the necessary tools
-        and platforms. Unauthorized individuals are strictly prohibited from
-        creating or modifying any Facebook Lead Ads on behalf of the company.
-        <br />
-        5. Ad Content and Targeting a. Ad Content: All Facebook Lead Ads must
-        adhere to our company's brand guidelines, tone of voice, and advertising
-        policies. The content should be accurate, truthful, and not misleading.
-        It should clearly communicate the value proposition and benefits of the
-        product or service being promoted. The use of offensive, discriminatory,
-        or inappropriate language, images, or videos is strictly prohibited. b.
-        Targeting: Facebook Lead Ads should be targeted to the appropriate
-        audience based on relevant criteria, such as demographics, interests, or
-        behaviors. It is essential to ensure that the target audience aligns
-        with the goals and objectives of the ad campaign.
-        <br />
-        6. Data Collection and Privacy a. Transparency: Facebook Lead Ads must
-        clearly disclose what information will be collected from users and how
-        it will be used. This includes providing a link to our company's privacy
-        policy and obtaining appropriate consent for data collection and
-        processing. b. Data Handling: Any data collected through Facebook Lead
-        Ads must be handled in accordance with our company's data protection and
-        privacy policies. Employees and representatives must ensure the security
-        and confidentiality of the data and take appropriate measures to prevent
-        unauthorized access, use, or disclosure. c. Data Retention: Collected
-        data should only be retained for as long as necessary to fulfill the
-        purpose for which it was collected or as required by applicable laws or
-        regulations. It should be securely deleted or anonymized once it is no
-        longer needed.
-        <br />
-        7. Monitoring and Reporting Loctech reserves the right to monitor and
-        review Facebook Lead Ads created by employees and representatives for
-        compliance with this policy. Any concerns or suspected violations should
-        be reported to the appropriate management or compliance personnel.
+        <p>
+          Purpose The purpose of this policy is to establish guidelines and
+          standards for the use of Facebook Lead Ads by employees and
+          representatives of Loctech. Facebook Lead Ads are an effective tool
+          for generating leads and capturing customer information, and it is
+          important that their use aligns with our company values and legal
+          requirements.
+        </p>
+
+        <p>
+          Scope This policy applies to all employees, contractors, and
+          representatives of Loctech who utilize Facebook Lead Ads for business
+          purposes.
+        </p>
+
+        <p>
+          Compliance with Laws and Regulations All Facebook Lead Ads created by
+          Loctech must comply with applicable laws, regulations, and guidelines,
+          including but not limited to data protection and privacy laws,
+          consumer protection laws, and advertising standards. It is the
+          responsibility of each employee or representative to ensure compliance
+          with these laws and regulations.
+        </p>
+
+        <p>
+          Authorized Use Facebook Lead Ads may only be created and managed by
+          authorized employees or representatives of Loctech who have received
+          appropriate training and have been granted access to the necessary
+          tools and platforms. Unauthorized individuals are strictly prohibited
+          from creating or modifying any Facebook Lead Ads on behalf of the
+          company
+        </p>
       </p>
     </div>
   );
